aeb0a23fb3a00825178ef85cf0e2c33dfe380298
Wan Muhammad Danish Amni Bin Wan Muhammad Zainuddin <18DDT23F1085@student.ptss.edu.my>
2026-03-29 19:06:13 +0800

    Update for result and style.

--- result.php
+++ result.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Keputusan Permohonan</title>
-    <link rel="stylesheet" href="style.css?v=1">
+    <link rel="stylesheet" href="style.css?v=2">
 </head>
 <body>
     <div class="container">
@@ -17,17 +17,43 @@
 
         <?php elseif (isset($_SESSION['success'])): ?>
             <div class="success-msg">
-                <strong><?php echo $_SESSION['success']; ?></strong><br><br>
-                
-                <b>Nama Penuh:</b> <?php echo $_SESSION['nama']; ?><br>
-                <b>No. Matrik:</b> <?php echo $_SESSION['no_matrik']; ?><br>
-                <b>Semester:</b> <?php echo $_SESSION['semester']; ?><br>
-                <b>Tarikh:</b> <?php echo $_SESSION['tarikh']; ?><br>
-                <b>Jabatan:</b> <?php echo $_SESSION['jabatan']; ?><br>
-                <b>Spesifikasi:</b> <?php echo $_SESSION['specs']; ?><br>
-                <b>Pengesahan:</b> <?php echo $_SESSION['pengesahan'] == 'Ya' ? 'Disahkan' : 'Tidak Disahkan'; ?><br>
-                <b>Alasan:</b> <?php echo $_SESSION['alasan']; ?><br>
+                <strong><?php echo $_SESSION['success']; ?></strong>
             </div>
+            
+            <table class="result-table">
+                <tr>
+                    <th>Nama Penuh</th>
+                    <td><?php echo $_SESSION['nama']; ?></td>
+                </tr>
+                <tr>
+                    <th>No. Matrik</th>
+                    <td><?php echo $_SESSION['no_matrik']; ?></td>
+                </tr>
+                <tr>
+                    <th>Semester</th>
+                    <td><?php echo $_SESSION['semester']; ?></td>
+                </tr>
+                <tr>
+                    <th>Tarikh Permohonan</th>
+                    <td><?php echo $_SESSION['tarikh']; ?></td>
+                </tr>
+                <tr>
+                    <th>Jabatan</th>
+                    <td><?php echo $_SESSION['jabatan']; ?></td>
+                </tr>
+                <tr>
+                    <th>Spesifikasi</th>
+                    <td><?php echo $_SESSION['specs']; ?></td>
+                </tr>
+                <tr>
+                    <th>Pengesahan Syarat</th>
+                    <td><?php echo $_SESSION['pengesahan'] == 'Ya' ? 'Disahkan' : 'Tidak Disahkan'; ?></td>
+                </tr>
+                <tr>
+                    <th>Alasan Sokongan</th>
+                    <td><?php echo nl2br($_SESSION['alasan']); ?></td>
+                </tr>
+            </table>
         <?php endif; ?>
 
         <div class="nav-link">
